3b4b76a6f03588059662bbd30e43dff8eac5994a
bryhmo <codewithbryhmo@gmail.com>
2023-05-11 14:51:22 -0500

    making the three dashboard for the user ,agent and the admin ,from the authenticationsessioncontroller.php and return

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    //this is the controller for the admincontroller for the admin only
+    public function AdminDashboard(){
+        return view('admin.admin_dashboard');
+
+    }//this is the end of the admin contoler method for the admin only 
 }
--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AgentController extends Controller
 {
-    //
+    //this is for the agent controller dashboard
+    public function AgentDashboard(){
+        return view('agent.agent_dashboard');
+    }//this is the end of the method 
 }
