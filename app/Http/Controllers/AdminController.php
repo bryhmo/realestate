376c021d8c5bd7f697428cef02281aa347c47582
bryhmo <codewithbryhmo@gmail.com>
2023-05-18 17:44:51 -0500

    working on the  admin register and logout code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+//this is the controller for the admincontroller for the admin only
 
 class AdminController extends Controller
 {
-    //this is the controller for the admincontroller for the admin only
+    
+
+ //the  Method  below control the admin profile logout 
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+    //this Method  return view to the admin dashboard 
     public function AdminDashboard(){
         return view('admin.admin_dashboard');
 
-    }//this is the end of the admin contoler method for the admin only 
+    }// End Method 
 
     public function AdminLogin(){
         return view('admin.admin_login');
     }//end method ... this is the method for the admin login 
+
+    public function AdminRegister(){
+        return view('admin.admin_register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,21 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::middleware(['auth','role:admin'])->group(function(){
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-});//this is the end of the route protected admin route from been accessed by the other users like agent and the users
 
+//the below  middleware prevent any other users from accesing the Admin Dashboard
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');//admin dashboard route ,protected 
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');//logout route ,protected 
+});
+
+
+//the below middelware prevent any other users from accesssing the agent dashboard
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });//this is the end of the route protected admin route from been accessed by the other users like agent and the users
 
-
+//the route below is the Admin dashboard login route , bc admin needs to login before been protected 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+//the route below is the Admin dashboard register route , bc admin needs to login before been protected 
+Route::get('/admin/register', [AdminController::class, 'AdminRegister'])->name('admin.register');
