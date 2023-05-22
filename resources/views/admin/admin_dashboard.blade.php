6c3631a945aeedb1bbb3be63f43cb8318a9a8ed4
bryhmo <codewithbryhmo@gmail.com>
2023-05-22 16:59:59 -0500

    ToastrJS is a JavaScript library for Gnome / Growl type non-blocking notifications

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('../assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
-
+    <link rel="stylesheet" type = "text/css" href = "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 </head>
 
 <body id="page-top">
@@ -433,6 +433,27 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{asset('../assets/js/sb-admin-2.min.js')}}"></script>
+
+    <script type ="text/javascript" src ="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if(session::has('message'))
+            var type = "{{session::get(''alert-type','info')}}"
+            switch(type){
+                case 'info':
+                toastr.info("{{session::get('message')}}");
+                break;
+                case 'success':
+                toastr.success("{{session::get('message')}}");
+                break;
+                case 'warning':
+                toastr.warning("{{session::get('message')}}");
+                break;
+                case 'error':
+                toastr.error("{{session::get('message')}}");
+                break;
+            }
+        @endif
+    </script>
 
 </body>
 
